597bb4998b7747575a72f907b45e0481aa248aa3
cheeter_Lee <johnson991210@gmail.com>
2023-01-14 18:00:18 +0800

    add popover in upload

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Navbar } from './Navbar'
 export { default as CustomDropzone } from './CustomDropzone'
-
--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,4 +1,5 @@
 import {
+    Box,
 	Button,
 	Flex,
 	Heading,
@@ -6,13 +7,27 @@ import {
 	Text,
 	Stack,
 	Image,
-    useToast,
+	useToast,
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverHeader,
+	PopoverFooter,
+	PopoverArrow,
+	PopoverCloseButton,
+	IconButton,
+	Portal,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbSeparator,
 } from "@chakra-ui/react"
 import React, { FormEvent, useState } from "react"
 import { CustomDropzone } from "../components"
 import { Web3Storage } from "web3.storage"
 import { useContractContext } from "../context/ContractContext"
 import { useWalletContext } from "../context/WalletContext"
+import { QuestionOutlineIcon, ChevronRightIcon } from "@chakra-ui/icons"
 
 const client = new Web3Storage({
 	token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNlOERBMTYyRmQyQjY5OTZlMWQzZDUyNmUwZmY0MTM4NGI5Q2ZiRmIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzM1NzYyMzQ0ODUsIm5hbWUiOiJldmlkZW5jZSJ9.toX_pod8vFLzoWDY3Ws2EbwqjjZRSTpyKYc2SKT3rP8",
@@ -21,18 +36,18 @@ const client = new Web3Storage({
 export interface IUploadPageProps {}
 
 const UploadPage: React.FunctionComponent<IUploadPageProps> = (props) => {
-    // chakra toast
-    const toast = useToast()
+	// chakra toast
+	const toast = useToast()
 	// upload image file captured from CustomDropzone
 	const [uploadImage, setUploadImage] = useState<any>(null)
 	// state for caption input
 	const [caption, setCaption] = useState("")
-    // state for loading
-    const [isLoading, setIsLoading] = useState(false)
-    // destructure from ContractContext
-    const { createImageAsset } = useContractContext()
-    // destructure from WalletContext
-    const { isConnected } = useWalletContext()
+	// state for loading
+	const [isLoading, setIsLoading] = useState(false)
+	// destructure from ContractContext
+	const { createImageAsset } = useContractContext()
+	// destructure from WalletContext
+	const { isConnected } = useWalletContext()
 
 	const uploadFile = async () => {
 		const rootCid = await client.put([uploadImage])
@@ -41,34 +56,34 @@ const UploadPage: React.FunctionComponent<IUploadPageProps> = (props) => {
 		const imageObjectBack = await res.files()
 		const { name } = imageObjectBack[0]
 		console.log("Image Url >>>", `https://${rootCid}.ipfs.w3s.link/${name}`)
-        const tempUrl = `https://${rootCid}.ipfs.w3s.link/${name}`
-        return tempUrl
+		const tempUrl = `https://${rootCid}.ipfs.w3s.link/${name}`
+		return tempUrl
 	}
 
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault()
 		console.log("submitting....")
-        setIsLoading(true)
-        try {
-            // upload file to ipfs
-            const imageUrl = await uploadFile()
-            // communicate with blockchain
-            console.log('caption', caption, 'url', imageUrl)
-            await createImageAsset(caption, imageUrl)
-            toast({
-                title: 'Successfully submitted :)',
-                status: 'success',
-                isClosable: true,
-            })
-        } catch (error) {
-            toast({
-                title: 'Failed to submit :(',
-                status: 'error',
-                isClosable: true,
-            })
-        }
-        setIsLoading(false)
-        setCaption('')
+		setIsLoading(true)
+		try {
+			// upload file to ipfs
+			const imageUrl = await uploadFile()
+			// communicate with blockchain
+			console.log("caption", caption, "url", imageUrl)
+			await createImageAsset(caption, imageUrl)
+			toast({
+				title: "Successfully submitted :)",
+				status: "success",
+				isClosable: true,
+			})
+		} catch (error) {
+			toast({
+				title: "Failed to submit :(",
+				status: "error",
+				isClosable: true,
+			})
+		}
+		setIsLoading(false)
+		setCaption("")
 	}
 
 	return (
@@ -95,7 +110,13 @@ const UploadPage: React.FunctionComponent<IUploadPageProps> = (props) => {
 					{uploadImage === null ? (
 						<CustomDropzone setUploadImage={setUploadImage} />
 					) : (
-						<Image p='2rem' w='400px' h='400px' objectFit='cover' src={URL.createObjectURL(uploadImage)} />
+						<Image
+							p="2rem"
+							w="400px"
+							h="400px"
+							objectFit="cover"
+							src={URL.createObjectURL(uploadImage)}
+						/>
 					)}
 
 					<Stack>
@@ -104,10 +125,40 @@ const UploadPage: React.FunctionComponent<IUploadPageProps> = (props) => {
 							onChange={(e) => setCaption(e.target.value)}
 							value={caption}
 						/>
-						<Button disabled={!isConnected} isLoading={isLoading} loadingText='submitting...' type="submit">Submit</Button>
+						<Button
+							disabled={!isConnected}
+							isLoading={isLoading}
+							loadingText="submitting..."
+							type="submit"
+						>
+							Submit
+						</Button>
 					</Stack>
 				</form>
 			</Flex>
+			<Popover>
+				<PopoverTrigger>
+					<IconButton
+						zIndex={20}
+						position="fixed"
+						right="10"
+						bottom="10"
+						aria-label="question"
+						icon={<QuestionOutlineIcon />}
+					/>
+				</PopoverTrigger>
+				<Portal>
+					<PopoverContent>
+						<PopoverArrow />
+						<PopoverHeader>Tips 💡</PopoverHeader>
+						<PopoverCloseButton />
+						<PopoverFooter>
+							Connect to crypto wallet first before uploading your
+							asset!
+						</PopoverFooter>
+					</PopoverContent>
+				</Portal>
+			</Popover>
 		</Flex>
 	)
 }
